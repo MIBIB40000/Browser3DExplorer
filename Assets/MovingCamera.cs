682e136f42e90c4b50ce3a322ec40cc168e13767
Konstantin <deev_konstantin97@mail.ru>
2021-06-09 16:29:30 +0500

    Update MovingCamera.cs

--- Assets/MovingCamera.cs
+++ Assets/MovingCamera.cs
@@ -8,54 +8,22 @@ public class MovingCamera : MonoBehaviour
     [SerializeField] Camera cam;
     [SerializeField] float speed = 2f;
 
-	public float startingFOV;
-
 	public float minPOV = 20f;
 	public float maxPOV = 70f;
-	public float zoomRate = 0.01f;
-
-	public float currentFOV;
-
-	public void Start()
-	{
-		startingFOV = cam.fieldOfView;
-		currentFOV = startingFOV;
-	}
 
 	void Update()
 	{
 		MakeZoom();
-		//if (Input.GetAxis("Mouse ScrollWheel") > 0)
-		//{
-		//	if (cam.fieldOfView > minPOV)
-		//	{
-		//		cam.fieldOfView--;
-		//	}
-		//}
-		//if (Input.GetAxis("Mouse ScrollWheel") < 0)
-		//{
-		//	if (cam.fieldOfView < maxPOV)
-		//	{
-		//		cam.fieldOfView++;
-		//	}
-		//}
+		MoveCamera();
+	}
 
+	public void MoveCamera()
+	{
 		if (Input.GetMouseButton(0))
 		{
 			transform.eulerAngles += -speed * new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
 		}
-
-		//if (Input.GetKeyDown(KeyCode.Space))
-		//{
-		//	currentFOV = startingFOV;
-		//}
-
-		//if (Input.GetKey(KeyCode.W))
-		//{
-		//	currentFOV += zoomRate;
-		//}
 	}
-
 	public void MakeZoom()
 	{
 		if (Input.GetAxis("Mouse ScrollWheel") > 0)
