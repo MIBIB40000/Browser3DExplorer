97f1baf82d445d498a209a8de55bd64cb232e619
Konstantin <deev_konstantin97@mail.ru>
2021-06-15 00:04:24 +0500

    Размещение объектов
    Работает размещение объекта на поверхности(без корректного определения положения на нем.

---
+++ Assets/Scripts/MovingCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCamera : MonoBehaviour
+{
+
+    [SerializeField] Camera cam;
+    [SerializeField] float speed = 2f;
+
+	public float minPOV = 20f;
+	public float maxPOV = 70f;
+
+	void Update()
+	{
+		MakeZoom();
+		MoveCamera();
+	}
+
+	public void MoveCamera()
+	{
+		if (Input.GetMouseButton(1))
+		{
+			transform.eulerAngles += -speed * new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+		}
+	}
+	public void MakeZoom()
+	{
+		if (Input.GetAxis("Mouse ScrollWheel") > 0)
+		{
+			if (cam.fieldOfView > minPOV)
+			{
+				cam.fieldOfView--;
+			}
+		}
+		if (Input.GetAxis("Mouse ScrollWheel") < 0)
+		{
+			if (cam.fieldOfView < maxPOV)
+			{
+				cam.fieldOfView++;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlacementController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementController : MonoBehaviour
+{
+    [SerializeField]private GameObject placeableObjectPrefab;
+	private GameObject currentObject;
+	private float mouseRotation;
+	private KeyCode objectCode = KeyCode.Space;
+
+	private void Start()
+	{
+		CreateObject();
+	}
+	private void Update()
+	{
+		
+
+		if(currentObject != null)
+		{
+			MoveCurrentObjectToMouse();
+			RotateFromMouse();
+			ReleaseIfClicked();
+		}
+	}
+
+	private void ReleaseIfClicked()
+	{
+		if (Input.GetKeyDown(objectCode))
+		{
+			currentObject = null;
+		}
+	}
+
+	private void RotateFromMouse()
+	{
+		mouseRotation += Input.mouseScrollDelta.y;
+		currentObject.transform.Rotate(Vector3.up, mouseRotation * 10f);
+	}
+
+	private void MoveCurrentObjectToMouse()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hitInfo;
+
+		if(Physics.Raycast(ray, out hitInfo))
+		{
+			currentObject.transform.position = hitInfo.point;
+			currentObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+		}
+	}
+
+	public void CreateObject()
+	{
+			currentObject = Instantiate(placeableObjectPrefab);
+	}
+
+}
